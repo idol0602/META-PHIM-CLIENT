18f535e1e5fc78accaa9c31eccfa1468262788d4
quanidol62 <nguyenquan06022004@gmail.com>
2025-05-10 13:24:13 +0700

    update 13:24 10/5/2025

--- movieApp/src/global/axiosInstance.js
+++ movieApp/src/global/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://meta-phim-server.onrender.com",
-  timeout: 10000,
+  timeout: 30000,
 });
 
 export default axiosInstance;
